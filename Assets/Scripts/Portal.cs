4035f29176429a9fae3612fa2729829d871aee53
DESKTOP-UFEEQ7V\pasca <Pascal.keller12@gmail.com>
2023-12-13 16:21:23 +0100

    joa nh

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -9,9 +9,42 @@ public class Portal : MonoBehaviour
     public Vector3 lastPos;
     public Quaternion lastRot;
 
+    public Collider leftCollider;
+    public Collider topCollider;
+    public Collider rightCollider;
+    Collider wallCollider;
+
+
     public UnityEvent<GameObject> resetPortal;
-   
-    
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Vector3.zero, transform.forward, out hit, 2f))
+            {
+                wallCollider = hit.collider;
+                wallCollider.enabled = false;
+            }
+
+
+            leftCollider.enabled = true;
+            topCollider.enabled = true;
+            rightCollider.enabled = true;
+        }
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            wallCollider.enabled = true;
+
+            leftCollider.enabled = false;
+            topCollider.enabled = false;
+            rightCollider.enabled = false;
+        }
+    }
 
     private void OnTriggerStay(Collider other)
     {
